7c81e20c3259c37b5980f32ea8a5e35283f181a0
Bryan Li <bryanli200282@gmail.com>
2024-09-23 08:36:50 -0400

    added requirements file and more read me

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # Sector_Rotation
 
+# Set up a env 
+```
+python3 -m venv .venv
+```
+## to activate
+```
+. .venv/bin/activate
+```
+
+## to deactivate
+```
+deactivate
+```
+
 ## How to install dependencies for jupyter notebook
 ```python
 import sys
--- gettingDate.py
+++ gettingDate.py
@@ -1,13 +1,12 @@
 import datetime
 import pandas as pd
-import pandas_datareader.data as web
 import yfinance as yf
 
 start = datetime.datetime(2019, 1, 1)
 end = datetime.datetime(2019, 5, 31)
 df_500 = yf.download('SPY', start=start, end=end)
 
-print(df_500.tail())
+print(df_500.tail()) 
 
 # how to calculate for Daily Return
 # Daily Return = (today close - yesterdays close)/yesterday close) * 100
